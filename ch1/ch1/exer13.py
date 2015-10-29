d26c00fc0e88383e238df9fdb31202683d51f9c8
Daniel Bove <dcbove@gmail.com>
2015-10-29 18:09:53 -0400

    Updated to do math with numpy

--- ch1/ch1/exer13.py
+++ ch1/ch1/exer13.py
@@ -14,11 +14,11 @@ from datetime import datetime
 boundxy = 50
 
 # build true function f
+# this is the true function we are attempting to model
 bias = 14
 w1 = 0.5
 w2 = 0.25
 
-# this is the true function we are attempting to model
 trueW = np.array([-bias, w1, w2])
 def f(x):
     value = np.dot(trueW, x)
@@ -39,75 +39,78 @@ def sign(x):
     else:
         return 0
     
-# try 50 times to find a misclassified example.  that'd be one where h(w, x) returns the wrong results for the current w
-def find_misclassifed_example(w, td):
+# try to find a misclassified example.  that'd be one where h(w, x) returns the wrong results for the current w
+def find_misclassifed_example(w, df):
     while (True):
-        i = random.randint(0, trainingDataCount - 1)
-        sampleData = td[i]
-        testResult = h(w, sampleData)
-        trainingDataResult = f(sampleData)
-        if (testResult != trainingDataResult):
-            return sampleData
+        i = random.randint(0, training_data_count - 1)
+        datum = df.ix[i]
+        training_data_result = datum.training_value
+        sample_data = [datum.w0, datum.w1, datum.w2]
+        testResult = h(w, sample_data)
+        if (testResult != training_data_result):
+            return np.array(sample_data)
 
 # given the training data, the known f, and current w, return % that match
-def calc_match_rate(trainingData, w):
-    match = 0
-    for td in trainingData:
-        if (f(td) == h(w, td)):
-            match += 1
-            
-    return match * 100 / trainingDataCount
+def calc_match_rate(dfTraining, w):
+    training_matrix = dfTraining.ix[:, ['w0', 'w1', 'w2']]
+    result = np.dot(w, training_matrix.T)
+    signed_result = np.array([sign(x) for x in result])
+    
+    training_values = dfTraining.ix[:, ['training_value']]
+    
+    # determine how many elements of signed_result and training_values are the same
+    tv = np.squeeze(np.asarray(training_values.T))
+    equal_count = len(signed_result[signed_result - tv == 0])
+    return (equal_count * 100) / training_data_count
 
 # training data
-trainingDataCount = 1000
-trainingData = []
-trainingValues = []
-for i in range(trainingDataCount):
-    datum = [1, random.randint(-boundxy, boundxy), random.randint(-boundxy, boundxy)]
-    trainingData.append(datum)
-    trainingValues.append(f(datum))
-    # print("Training Datum {}, Sign {}".format(datum, f(datum)))
-trainingData = np.array(trainingData)
-trainingValues = np.array(trainingValues)
+training_data_count = 1000
+df_training = pd.DataFrame(index=np.arange(0, training_data_count), columns=['w0', 'w1', 'w2', 'training_value'])
+for i in range(training_data_count):
+    w = [1, random.randint(-boundxy, boundxy), random.randint(-boundxy, boundxy)]
+    trainingValue = f(w)
+    df_training.loc[i] = [w[0], w[1], w[2], trainingValue]
 
 # opening value for w
-w = np.array([0, 0, 0])
+w = np.array([1, 1, 1])
 
 # training iterations
 t_iters = 10000
 
 # train
-matchHistory = []
+match_history = []
 stopwatch = datetime.now()
 for t in range(t_iters):
-    match_rate = calc_match_rate(trainingData, w)
+    match_rate = calc_match_rate(df_training, w)
+    match_history.append(match_rate)
     if match_rate > 98:
         print('found success. w={}, Match Rate={} '.format(w, match_rate))
         break
     
-    matchHistory.append(match_rate)
     
     if t % 50 == 0:
-        print("iter: {}, timediff: {}.{}".format(t, (datetime.now()-stopwatch).seconds, (datetime.now()-stopwatch).microseconds))
+        print("iter: {}, timediff: {}.{}".format(t, (datetime.now() - stopwatch).seconds, (datetime.now() - stopwatch).microseconds))
         stopwatch = datetime.now()
     
-    misclassified_example = find_misclassifed_example(w, trainingData)
+    misclassified_example = find_misclassifed_example(w, df_training)
     if (misclassified_example == None):
         print('could not find misclassified example. w={}, Match Rate={} '.format(w, match_rate))
         break
     else:
         y = f(misclassified_example)
-        # print('step {}, w={}, y={}, mce={}, u={}, new w={}, Match Rate={}'.format(t, w, y, misclassified_example, (y * misclassified_example), np.add(w, y * misclassified_example), calc_match_rate(trainingData, w)))
         w = w + (y * misclassified_example)
 
-print("final w ", w, match_rate)
-
 # plot training data
+training_values = np.squeeze(np.array(df_training.ix[:, ['training_value']]))
+training_xs = np.squeeze(np.array(df_training.ix[:, ['w1']].T))
+training_ys = np.squeeze(np.array(df_training.ix[:, ['w2']].T))
+
 f, (ax1, ax2) = plt.subplots(1, 2)
-color = [str((x + 1) / 2) for x in trainingValues]
+
+color = [str((x + 1) / 2) for x in training_values]
 plt.axhline(0, color='gray')
 plt.axvline(0, color='gray')
-ax1.scatter(trainingData.T[1], trainingData.T[2], c=color)
+ax1.scatter(training_xs, training_ys, c=color)
 
 # for w, determine x2 at x1=10, x1=-10
 leftx2 = (-w[0] - w[1] * (-boundxy)) / w[2]
@@ -116,7 +119,7 @@ l = Line2D([-boundxy, boundxy], [leftx2, rightx2])
 ax1.add_line(l)
 
 # plot training match history
-series = pd.Series(matchHistory, index=np.arange(0, len(matchHistory)))
+series = pd.Series(match_history, index=np.arange(0, len(match_history)))
 series.plot()
  
 plt.show()
